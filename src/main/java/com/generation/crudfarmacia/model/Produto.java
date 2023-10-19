77a850771cacfa575bc52d66cacef5b6792adec5
Elisa Souza <contato.elisasouza@hotmail.com>
2023-10-19 14:42:37 -0300

    first commit

---
+++ src/main/java/com/generation/crudfarmacia/model/Produto.java
@@ -0,0 +1,97 @@
+package com.generation.crudfarmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo nome é obrigatório")
+    @Size(max = 100, message = "O atributo nome pode ter no máximo 100 caractéres")
+    private String nome;
+
+    @NotBlank(message = "O atributo descricao é obrigatório")
+    private String descricao;
+
+    @NotNull(message = "O atributo preço é obrigatório")
+    private BigDecimal preco;
+
+    @NotNull(message = "O atributo quantidade é obrigatório")
+    @PositiveOrZero
+    private Integer quantidade;
+
+    @UpdateTimestamp
+    private LocalDateTime data;
+
+    @ManyToOne
+    @JsonIgnoreProperties("produto")
+    private Categoria categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
