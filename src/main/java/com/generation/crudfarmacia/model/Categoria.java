b663d1bfdc26d85ed4948a3e0614eabe566f5f98
Elisa Souza <contato.elisasouza@hotmail.com>
2023-10-19 14:36:38 -0300

    first commit

---
+++ src/main/java/com/generation/crudfarmacia/model/Categoria.java
@@ -0,0 +1,31 @@
+package com.generation.crudfarmacia.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo tipo é obrigatório")
+    private String tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
